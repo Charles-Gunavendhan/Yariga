0c547f0f7951a2bd2a3e0a6a42c2acb6741bdc2c
Charles-GunaVendhan <charlesxced@gmail.com>
2024-01-30 21:49:50 +0530

    First commit

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import yariga from './yariga.svg';
+
+// export {
+//   logo,
+//   yariga,
+// };
+import React from 'react'
+
+export const index = () => {
+  return (
+    <div>index</div>
+  )
+}
